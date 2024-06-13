208495310246b8fca45ec51e4e11d06db4a39a2e
Yazan Soudan <68210928+MidanGhost@users.noreply.github.com>
2024-06-13 12:42:51 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import xmlrpc.client
 app = Flask(__name__)
+url = "http://alhorae.odoo.com:80"
+db = "odooerp-ae-alhor1-main-4747584"
+email = "yazansodan@gmail.com"
+password = "15c033e53611eb0b8827e14aecd505601850e906"
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Koyeb'
+common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+print(common.version())
 
-
-if __name__ == "__main__":
-    app.run()
