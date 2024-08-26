35fee13af27124ccf0ac995a34cdf4cab0342e58
daedaluschan <daedalus.chan@gmail.com>
2024-08-26 21:25:35 +0100

    Ignore about api key validation for now

--- app.py
+++ app.py
@@ -4,18 +4,18 @@ import os
 app = Flask(__name__)
 
 # Read API Key from environment variable
-VALID_API_KEYS = {os.getenv('API_KEY')}  # Assuming there's only one key for simplicity
+# VALID_API_KEYS = {os.getenv('API_KEY')}  # Assuming there's only one key for simplicity
 
-def require_api_key(f):
-    def decorated(*args, **kwargs):
-        api_key = request.headers.get('API-Key')
-        if api_key not in VALID_API_KEYS:
-            abort(401)  # Unauthorized access if the API key is not valid
-        return f(*args, **kwargs)
-    return decorated
+# def require_api_key(f):
+#     def decorated(*args, **kwargs):
+#         api_key = request.headers.get('API-Key')
+#         if api_key not in VALID_API_KEYS:
+#             abort(401)  # Unauthorized access if the API key is not valid
+#         return f(*args, **kwargs)
+#     return decorated
 
 @app.route('/')
-@require_api_key
+# @require_api_key
 def hello_world():
     return jsonify(message="Hello, Happy Flasking!")
 
