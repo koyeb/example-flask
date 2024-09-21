0cc1308cbf5893625c4fa5e4bef3d69e4d891e29
daedaluschan <daedalus.chan@gmail.com>
2024-09-21 23:25:37 +0100

    print out the parsed time value

--- tariff_utils.py
+++ tariff_utils.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 import requests
+from dateutil import parser 
 
 def calculate_start_time(num_hours, api_key):
     """
@@ -16,7 +17,22 @@ def calculate_start_time(num_hours, api_key):
     if response.status_code == 200:
         # Parse the JSON response
         data = response.json()
-        print(data)
+        tariff_data = response.json()['results']
+        for slot in tariff_data:
+            # Using `dateutil.parser.parse` to correctly handle timezone information
+            valid_from = parser.isoparse(slot['interval_start'])  # Parses with timezone info
+            valid_to = parser.isoparse(slot['interval_end'])  # Parses with timezone info
+            
+            # Convert both valid_from and valid_to to UTC
+            valid_from_utc = valid_from.astimezone(tz=datetime.timezone.utc)
+            valid_to_utc = valid_to.astimezone(tz=datetime.timezone.utc)
+
+            print ('---------------')
+            print(f'from datetime{valid_from}')
+            print(f'to datetime{valid_to}')
+            print(f'from utc{valid_from_utc}')
+            print(f'to utc{valid_to_utc}')
+
     else:
         print(f'Error: {response.status_code} - {response.reason}')
 
