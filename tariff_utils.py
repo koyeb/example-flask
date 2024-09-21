cce95ed647f46d7267464b618bb8e846ffe1bcc4
daedaluschan <daedalus.chan@gmail.com>
2024-09-21 17:15:44 +0100

    change to simulate curl -u on the request.get()

--- tariff_utils.py
+++ tariff_utils.py
@@ -2,9 +2,9 @@ from datetime import datetime, timedelta
 import requests
 import os
 
-
-api_url = 'https://api.octopus.energy/v1/electricity-meter-points/1200036570277/meters/18L2047503/consumption/'
 api_auth = os.environ.get("octupus-key")
+api_url = 'https://' + api_auth + 'api.octopus.energy/v1/electricity-meter-points/1200036570277/meters/18L2047503/consumption/'
+
 
 def calculate_start_time(num_hours):
     """
@@ -14,7 +14,8 @@ def calculate_start_time(num_hours):
     :return: str - The start time formatted as 'YYYY-MM-DD HH:MM:SS'
     """
 
-    response = requests.get(api_url, auth=api_auth)
+    print(api_url)
+    response = requests.get(api_url)
     print(response)
 
     current_time = datetime.now()
