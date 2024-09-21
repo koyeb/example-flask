b1b30da238966af147554376b56c4806ae90b30c
daedaluschan <daedalus.chan@gmail.com>
2024-09-21 23:51:11 +0100

    switch back

--- tariff_utils.py
+++ tariff_utils.py
@@ -19,19 +19,13 @@ def calculate_start_time(num_hours, api_key):
         data = response.json()
         tariff_data = response.json()['results']
         for slot in tariff_data:
-            # Using `dateutil.parser.parse` to correctly handle timezone information
-            valid_from = parser.isoparse(slot['valid_from'])  # Parses with timezone info
-            valid_to = parser.isoparse(slot['valid_to'])  # Parses with timezone info
-            
-            # Convert both valid_from and valid_to to UTC
-            valid_from_utc = valid_from.astimezone(tz=datetime.timezone.utc)
-            valid_to_utc = valid_to.astimezone(tz=datetime.timezone.utc)
+            # Parse the date strings assuming they are in UTC
+            valid_from = datetime.fromisoformat(slot['valid_from'].replace('Z', '+00:00'))
+            valid_to = datetime.fromisoformat(slot['valid_to'].replace('Z', '+00:00'))
 
             print ('---------------')
             print(f'from datetime{valid_from}')
             print(f'to datetime{valid_to}')
-            print(f'from utc{valid_from_utc}')
-            print(f'to utc{valid_to_utc}')
 
     else:
         print(f'Error: {response.status_code} - {response.reason}')
