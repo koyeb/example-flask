9614067de317a25d0b6a06c3865c0c1094629f27
daedaluschan <daedalus.chan@gmail.com>
2024-09-21 16:50:45 +0100

    try to run request.get

--- tariff_utils.py
+++ tariff_utils.py
@@ -1,4 +1,8 @@
 from datetime import datetime, timedelta
+import requests
+
+api_url = 'https://api.octopus.energy/v1/electricity-meter-points/1200036570277/meters/18L2047503/consumption/'
+api_auth = 'sk_live_6yKY1YmWVNJDN4sK7paEIYTi'
 
 def calculate_start_time(num_hours):
     """
@@ -7,6 +11,10 @@ def calculate_start_time(num_hours):
     :param num_hours: int - Number of hours to add to the current time
     :return: str - The start time formatted as 'YYYY-MM-DD HH:MM:SS'
     """
+
+    response = requests.get(api_url, auth=api_auth)
+    print(response)
+
     current_time = datetime.now()
     start_time = current_time + timedelta(hours=num_hours)
     return start_time.strftime('%Y-%m-%d %H:%M:%S')
